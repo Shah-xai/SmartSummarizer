80d14aab12a01c8fc4c5cc146dca1e2238b84997
Shahriyar <sh.abedinnejad@gmail.com>
2025-11-10 16:22:04 -0800

    Training completed on AWS

---
+++ src/entity/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from pathlib import Path
+@dataclass
+class DataIngestionConfig:
+    root_dir:Path
+    source_url:str
+    local_data_file:Path
+
+@dataclass
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: str
+
+@dataclass
+class ModelTrainingConfig:
+  root_dir: Path
+  data_dir: Path
+  model_name: str
+  training_params: dict
+  
+
+
+
+
+    
